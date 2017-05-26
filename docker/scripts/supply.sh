9e196a7cb4971bcea2670ddf0d44152870c2a6bf
Jan Sanchez <joejansanchez@gmail.com>
2017-05-26 15:38:10 -0500

    feat scripts: actualizo supply

--- docker/scripts/supply.sh
+++ docker/scripts/supply.sh
@@ -9,5 +9,6 @@ fi
 if [ -f ./db/frontend_wp.sql.zip ]; then
 	echo "zipear"
 	unzip -f ./db/frontend_wp.sql.zip -d ./db/
-	docker-compose -f ./docker/compose/supply-blog.yml run --rm import
+	docker-compose up -d mysql
+	#cat ./db/frontend_wp.sql | docker exec --privileged -i dockerfrontendlabs_mysql_1 /usr/bin/mysql -u root --password=123456 frontend_wp
 fi
