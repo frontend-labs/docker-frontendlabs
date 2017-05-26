9c2042c543f2fcdb70eac5650158902ac34e9024
Jan Sanchez <joejansanchez@gmail.com>
2017-05-26 16:01:14 -0500

    feat scripts: agrego sleep de 15 segundos

--- docker/scripts/supply.sh
+++ docker/scripts/supply.sh
@@ -7,8 +7,13 @@ if [ ! -d .volumes ]; then
 fi
 
 if [ -f ./db/frontend_wp.sql.zip ]; then
-	echo "zipear"
+	echo "unzipping..."
 	unzip -f ./db/frontend_wp.sql.zip -d ./db/
 	docker-compose up -d mysql
-	#cat ./db/frontend_wp.sql | docker exec --privileged -i dockerfrontendlabs_mysql_1 /usr/bin/mysql -u root --password=123456 frontend_wp
+	echo "Running Mysql..."
+	sleep 15
+	echo "all Done."
+	cat ./db/frontend_wp.sql | docker exec --privileged -i dockerfrontendlabs_mysql_1 /usr/bin/mysql -u root --password=123456 frontend_wp
+	docker-compose stop mysql
+	docker-compose down
 fi
